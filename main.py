9a03941b7e446a967ffdd9cedee141997221bf10
Tomeriko-king <tomerklein9@gmail.com>
2025-01-03 11:54:36 +0200

    Use functions in main

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
+from tcp_connection import start_tcp_connection
+from hand_capture import capture_video
+
+
+def main():
+    start_tcp_connection()
+    capture_video()
+
+
 if __name__ == '__main__':
-    print('VOSS-Client')
+    main()
